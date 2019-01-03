aef233f216a8119310f6dac619eb4c66d78bc44f
Steven Victor <chikodi543@gmail.com>
2019-01-03 14:39:35 +0100

    modified setup

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 //mongoose uses callback by default. but we want the javascript global promise
 mongoose.Promise = global.Promise;
 //unlike normal connect, where need to use callback to get our connected database work:
-mongoose.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
 
 module.exports = {
     mongoose
